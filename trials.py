092bec8c5696410bf1e959ae815a6e769f6c3a17
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-04-29 00:02:33 +0000

    Added functionality for snake_to_camel, longest_word

--- trials.py
+++ trials.py
@@ -4,7 +4,6 @@ def output_all_items(items):
     for item in items:
         print(item)
 
-
 output_all_items([1, 'hello', True])
 
 
@@ -14,7 +13,6 @@ def get_all_evens(nums):
         if num % 2 == 0: 
             even_nums.append(num)
     return even_nums
-
 
 nums = [7, 8, 10, 1, 2, 2]
 print(get_all_evens(nums))
@@ -29,14 +27,12 @@ def get_odd_indices(items):
 
     return result
 
-
 print(get_odd_indices([1, 'hello', True, 500]))
 
 
 def print_as_numbered_list(items):
     for index, item in enumerate(items): 
         print (f"{index+1}. {item}")
-
 
 print_as_numbered_list([1, 'hello', True])
 
@@ -48,7 +44,6 @@ def get_range(start, stop):
         nums.append(number)
 
     return nums
-
 
 print(get_range(0, 5))           
 
@@ -65,35 +60,30 @@ def censor_vowels(word):
     
 print(censor_vowels('hello world!'))
 
-# function snakeToCamel(string) {
-#   const camelCase = [];
 
-#   for (const word of string.split('_')) {
-#     camelCase.push(`${word[0].toUpperCase()}${word.slice(1)}`);
-#   }
-
-#   return camelCase.join('');
-# }
-
-#def snake_to_camel(string):
-    # camel_case = []
-
-    # words = string.split('_')
+def snake_to_camel(string):
     
-    
-    # for word in words:
-        
-    #     camel_case.append(word[0].upper())
-    #     else: 
+    camel_case = []
 
+    words = string.split('_')
+    for word in words:
+        camel_case.append(f"{word[0].upper()}{word[1:]}") 
 
+    return (' ').join(camel_case)
 
-    # print(('').join(camel_case))
+print(snake_to_camel('hello_world'))
 
-#snake_to_camel('hello_world')
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    longest = len(words[0])
+
+    for word in words:
+        if longest < len(word):
+            longest = len(word)
+
+    return longest
+
+print(longest_word_length(['jellyfish', 'zebra']))
 
 
 def truncate(string):
@@ -107,10 +97,3 @@ def has_balanced_parens(string):
 def compress(string):
     pass  # TODO: replace this line with your code
 
-
-# word = 'Hello Word'
-# words = word.split('_')
-# print (words)
-# for i in words:
-#     for n in i: 
-#         print(n)
